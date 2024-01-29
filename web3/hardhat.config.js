3c4343e793565589c2176ab45e35f02846a66050
username <email@email.email>
2024-01-29 19:11:53 +0300

    Update hardhat.config.js

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -12,18 +12,11 @@ module.exports = {
     },
   },
   networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
-    },
+   hardhat: {},
+   goerli:{ 
+    url: 'https://rpc.ankr.com/eth_goerli',
+    accounts: ['0x${process.env.PRIVATE_KEY}']
+   }
   },
   paths: {
     artifacts: "./artifacts-zk",
@@ -33,6 +26,7 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: 'goerli',
     settings: {
       optimizer: {
         enabled: true,
