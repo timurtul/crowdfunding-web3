117ef042f7cc1d31ff0e254068ef843d5a3ba594
username <email@email.email>
2024-01-29 16:57:18 +0300

    Update CrowdFunding.sol

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -45,8 +45,8 @@ contract CrowdFunding {
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
 
-        (bool sent) = payable(campaign.owner).call{value: amount}("");
-        if[sent]{
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+        if(sent){
             campaign.amountCollected = campaign.amountCollected + amount;
         }
     }
@@ -57,5 +57,13 @@ contract CrowdFunding {
 
     function getCampaigns() public view returns(Campaign[] memory){
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+            
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
     }
 }
